5c1cc27400012c2f7f5c3ef9ac962c65c5f3bc3d
JaeWonSong <jwsonghub@gmail.com>
2023-06-23 22:08:52 +0900

    230623_22:00

--- ACDD/ACDD/settings.py
+++ ACDD/ACDD/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'acdd',
-        'USER': 'jeawon',
+        'USER': 'wooyoung',
         'PASSWORD': '1735',
         'HOST': '192.168.50.131',
         'PORT': '3306',
@@ -138,9 +138,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# host_name = socket.gethostname()
-# HOST = socket.gethostbyname(host_name)
-# PORT = 9999
+host_name = socket.gethostname()
+HOST = socket.gethostbyname(host_name)
+PORT = 9999
 
-# thread = threading.Thread(target=Server, args=(HOST, PORT))
-# thread.start()
+thread = threading.Thread(target=Server, args=(HOST, PORT))
+thread.start()
